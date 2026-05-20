fc20eecd1fc18c585abb209ff4c53064f987d51d
ncfb <115941990+ncfb@users.noreply.github.com>
2026-05-20 23:33:08 +0800

    Add settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.aliyun.com/repository/google") }
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "v2rayNG"
+include(":app")
