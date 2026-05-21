62277dad0b75c55a0df361909e890943763e070b
ncfb <115941990+ncfb@users.noreply.github.com>
2026-05-21 11:15:28 +0800

    Fix: build config for GitHub Actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,13 @@
 pluginManagement {
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
         google()
         mavenCentral()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/public") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
         google()
         mavenCentral()
         maven { url = uri("https://jitpack.io") }
