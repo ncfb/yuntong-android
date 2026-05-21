0a635aae3e66eebd52b4b22fa0fea3e2a1833fd8
ncfb <115941990+ncfb@users.noreply.github.com>
2026-05-21 11:34:53 +0800

    Fix: remove kotlin plugin (bundled in AGP 9)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,5 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Top-level build file 
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
-}
-
-buildscript {
-    dependencies {
-        classpath(libs.gradle.license.plugin)
-    }
 }
